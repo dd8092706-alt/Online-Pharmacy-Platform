ee2388f84ebcffac5f242f59b67bdbb96e54b2d3
Divya Dharshini <dd8092706@gmail.com>
2026-08-27 20:09:53 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -12,3 +12,20 @@ def register(name, email, password):
         return False
     finally:
         db.close()
+        def login(email, password):
+    db = connect()
+    cur = db.cursor()
+    cur.execute(
+        "SELECT id,name,email,role FROM users WHERE email=? AND password=?",
+        (email, password)
+    )
+    row = cur.fetchone()
+    db.close()
+    if row:
+        return {
+            "id": row[0],
+            "name": row[1],
+            "email": row[2],
+            "role": row[3]
+        }
+    return None
