cb1498f17b4ec3c50d5839c2e20fc88b895ae997
Divya Dharshini <dd8092706@gmail.com>
2026-09-01 10:35:39 +0530

    Update Pharmacy.py

--- Pharmacy.py
+++ Pharmacy.py
@@ -1,1 +1,23 @@
-
+from database import connect
+def medicines(search=""):
+    db = connect()
+    cur = db.cursor()
+    if search:
+        cur.execute(
+            "SELECT * FROM medicines WHERE name LIKE ?",
+            ("%" + search + "%",)
+        )
+    else:
+        cur.execute("SELECT * FROM medicines")
+    rows = cur.fetchall()
+    db.close()
+    return [
+        {
+            "id": r[0],
+            "name": r[1],
+            "category": r[2],
+            "price": r[3],
+            "stock": r[4]
+        }
+        for r in rows
+    ]
